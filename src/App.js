527bdf1f025d14776084fdc881b3e905f9ad1111
pevescarol <carol.peves@gmail.com>
2023-07-04 13:13:07 -0300

    Revert "Merge pull request #1 from leodesanmiguel/about-section"
    This reverts commit 1fd8746687a57a0a6da0ebef24796f39e9b25988, reversing
    changes made to 7ebad9aa2f9c2bdac5e2b289c6f0c221cc3cdc8b.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
 import { AboutUs } from "./pages/About-us/AboutUs";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="*" element={<Home />}></Route>
           <Route path="home" element={<Home />}></Route>
