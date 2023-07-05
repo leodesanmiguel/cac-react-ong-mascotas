fad1914613945326bb6d9c6370bca1bdb41a0d82
MarisaSeg <mesthor683@gmail.com>
2023-07-05 11:44:33 -0300

    cambios

--- src/components/History-Card/Card.jsx
+++ src/components/History-Card/Card.jsx
@@ -2,72 +2,51 @@ import React from 'react';
 import "react-multi-carousel/lib/styles.css";
 import Carousel from "react-multi-carousel";
 import './Card.css';
-
-//importando las imágenes del carousel
-import uno from '../../assets/img-cards/uno.jpg';
-import dos from '../../assets/img-cards/dos.jpg';
-import tres from '../../assets/img-cards/tres.jpg';
-import cuatro from '../../assets/img-cards/cuatro.jpg';
-
-const cards = [
-    {
-        img: uno,
-        text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        title:"Boby"
-    },
-    {
-        img: dos,
-        text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        title:"Reina"
-    },
-    {
-        img: tres,
-        text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        title:"Peter"
-    },
-    {
-        img: cuatro,
-        text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-        title:"Tomy"
-    }
-]
+import { dogs } from '../../data/dogs';
 
 
 
-export const Card = (img, text, title ) => {
+
+
+function Card( img, text, title){
+   
     const responsive = {
-        //Responsive
+        
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
           items: 3
         },
+        laptop : {
+            breakpoint: { max: 1024, min: 970 },
+            items: 3
+        },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 3
+          breakpoint: { max: 969, min: 690 },
+          items: 2
     
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
+          breakpoint: { max: 689, min: 464 },
           items: 1
         }
       };
     return(
         <>
-            <div className="contenedor">
+            <div className="contiene-cards">
                 
                 <Carousel responsive={responsive}>
-                    {
-                        cards.map(card => {
+                    {   
+                        dogs.map(dog => {
                             return(
                                 
-                                    <div className="card">
+                                    <div className="card container-sm">
                                         <div className="img-cover">
-                                            <img src={ card.img } className="card-img-top"  alt="foto de" />                   
+                                            <img src={ dog.img } className="card-img-top"  alt={ dog.title } />                   
                                         </div>   
                                         <div className="card-body">
-                                            <h5 className="card-title mb-2">{ card.title }</h5>
-                                            <p className="card-text">{ card.text }</p>
-                                            <a href="/" className="btn" >Ver Historia</a>
+                                            <h5 className="card-title mb-2">{ dog.title }</h5>
+                                            <p className="card-text">{ dog.text }</p>
+                                            <a href="/" className="btn btn-ligth btn-card" >Ver Historia</a>
                                         </div>
                                     </div>
                                    
@@ -82,3 +61,5 @@ export const Card = (img, text, title ) => {
         </>
     );
 };
+
+export default Card;
